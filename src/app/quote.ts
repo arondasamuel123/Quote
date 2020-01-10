848f5151bde66e40f2c09f8a47eeb203ffb3ff8b
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-10 11:29:55 +0300

    Made changes to the Quote class and added a form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
 
-  constructor(public quote: string, public author: string, public upvote: number, public downvote: number) {
+  constructor(public quote: string, public author: string, public submitedBy:string, public upvote: number, public downvote: number) {
 
   }
 }
