c8c71c1020ae1b00d3c26e56f45fa508d981d794
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-11 20:02:24 +0300

    Created and initialized pipe and used it in the quote component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateAgoPipe } from './date-ago.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DateAgoPipe
   ],
   imports: [
     BrowserModule,
