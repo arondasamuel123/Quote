396521956a3db0c00af730b2c2d32a58a86daa41
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-11 19:03:10 +0300

    Added a date attribute to the Quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
   public votes: number;
   public upvoted: boolean;
   public downvoted: boolean;
-  constructor(public quote: string, public author: string, public submitedBy: string) {
+  constructor(public quote: string, public author: string, public submitedBy: string, datePosted: Date) {
     this.votes = 0;
     this.upvoted = false;
     this.downvoted = false;
