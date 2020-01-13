99ea4c8e0c72af29b8a1467848161f59a234e0cb
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-13 11:28:37 +0300

    Changed styling using bootstrap in quote component.html and app component as well as highlighting quote with calcuating the votes

--- src/app/text-highlight.directive.ts
+++ src/app/text-highlight.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appTextHighlight]'
 })
 export class TextHighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+
+    this.elem.nativeElement.style.backgroundColor = '#68FF33';
+
+   }
+
+
 
 }
